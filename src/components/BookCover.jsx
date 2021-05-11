2e70d4d8c56eeb53ca5a82d3214650a3f08c3481
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-05-11 17:01:29 +0200

    Optimized carousel and chose which one to use

--- src/components/BookCover.jsx
+++ src/components/BookCover.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './BookCover.css'
 export default function BookCover(props) {
-
+    
+    const num = Number(props.number) + 1
+    console.log(num)
+    console.log(props.data)
     return (
-        <div>
+        <div data-slide={num} className="product">
             <Link to={`/collection/${props.data._id}`}>
 
-            <div><img src={props.data.image} alt="hehe" /></div>
-              <h5>{props.data.title} </h5>
+                <span>
+                    <img className='cover' src={props.data.image} alt="hehe" />
+                </span>
+                <h5>{props.data.title} </h5>
             </Link>
               
         </div>
--- src/components/Carousel2.jsx
+++ src/components/Carousel2.jsx
@@ -1,50 +1,61 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import './Carousel2.css'
+import { DataContext } from '../context/DataContext'
+import BookCover from './BookCover';
 
 export default function Carousel2() {
-    var carousels = document.querySelectorAll('.js-product-carousel');
-    if (carousels.length > 0) {
-        [].forEach.call(carousels, function (carousel) {
-            carouselize(carousel);
-        });
-    }
+    
+    const { collection } = useContext(DataContext)
 
-    function carouselize(carousel) {
-        var productList = carousel.querySelector('.js-product-list');
-        var productListWidth = 0;
-        var productListSteps = 0;
-        var products = carousel.querySelectorAll('.product');
-        var productAmount = 0;
-        var productAmountVisible = 3;
-        var carouselPrev = carousel.querySelector('.js-carousel-prev');
-        var carouselNext = carousel.querySelector('.js-carousel-next');
+    if (collection) {
+        var carousels = document.querySelectorAll('.js-product-carousel');
+        if (carousels.length > 0) {
+            [].forEach.call(carousels, function (carousel) {
+                carouselize(carousel);
+            });
+        }
+    
+        function carouselize(carousel) {
 
-        //Count all the products
-        [].forEach.call(products, function (product) {
-            productAmount++;
-            productListWidth += 80;
-            productList.style.width = productListWidth + "vw";
-        });
-
-        carouselNext.onclick = function () {
-            if (productListSteps < productAmount - productAmountVisible) {
-                productListSteps++;
-                moveProductList();
+            var productList = carousel.querySelector('.js-product-list');
+            var productListWidth = 80;
+            var productListSteps = 0;
+            var productAmount = 0;
+            var productAmountVisible = 4;
+            var carouselPrev = carousel.querySelector('.js-carousel-prev');
+            var carouselNext = carousel.querySelector('.js-carousel-next');
+    
+            //Count all the products
+            [].forEach.call(collection, function (product) {
+                productAmount++;
+                productListWidth += 80;
+                productList.style.width = productListWidth + "vw";
+            });
+    
+            carouselNext.onclick = function () {
+                console.log(productListSteps)
+                console.log(productAmount)
+                console.log(productAmountVisible)
+                if (productListSteps < productAmount - productAmountVisible ) {
+                    productListSteps++;
+                    moveProductList();
+                }
+            }
+            carouselPrev.onclick = function () {
+                if (productListSteps > 0) {
+                    productListSteps--;
+                    moveProductList();
+                }
+            }
+    
+            // This is a bit hacky, let me know if you find a better way to do this!
+            // Move the carousels product-list
+            function moveProductList() {
+                productList.style.transform = "translateX(-" + 20 * productListSteps + "vw)";
             }
         }
-        carouselPrev.onclick = function () {
-            if (productListSteps > 0) {
-                productListSteps--;
-                moveProductList();
-            }
-        }
-
-        // This is a bit hacky, let me know if you find a better way to do this!
-        // Move the carousels product-list
-        function moveProductList() {
-            productList.style.transform = "translateX(-" + 20 * productListSteps + "vw)";
-        }
     }
+   
     return (
         <div>
             <div className="carousel js-product-carousel">
@@ -52,36 +63,13 @@ export default function Carousel2() {
                     <span className="carousel__control js-carousel-prev"><i className="icon">previous</i></span>
                     <span href="#" className="carousel__control js-carousel-next"><i className="icon">next</i></span>
                     <ul className="product-list js-product-list">
-                        <li className="product-list__item">
-                            <div data-slide="1" className="product">
-                                <span>1</span>
-                            </div>
-                        </li>
-                        <li className="product-list__item">
-                            <div data-slide="2" className="product">
-                                <span>2</span>
-                            </div>
-                        </li>
-                        <li className="product-list__item">
-                            <div data-slide="3" className="product">
-                                <span>3</span>
-                            </div>
-                        </li>
-                        <li className="product-list__item">
-                            <div data-slide="4" className="product">
-                                <span>4</span>
-                            </div>
-                        </li>
-                        <li className="product-list__item">
-                            <div data-slide="5" className="product">
-                                <span>5</span>
-                            </div>
-                        </li>
-                        <li className="product-list__item">
-                            <div data-slide="6" className="product">
-                                <span>6</span>
-                            </div>
-                        </li>
+                        {collection && Object.entries(collection).map((element) => {
+                            return  <li key={element[0]} className="product-list__item">
+                                        <BookCover number={element[0]} data={element[1]}/>
+                                    </li>
+                        })
+
+                        }
                     </ul>
                 </div>
             </div>
@@ -115,11 +103,7 @@ export default function Carousel2() {
                                 <span>5</span>
                             </div>
                         </li>
-                        <li className="product-list__item">
-                            <div data-slide="6" className="product">
-                                <span>6</span>
-                            </div>
-                        </li>
+
                     </ul>
                 </div>
             </div>
@@ -153,11 +137,7 @@ export default function Carousel2() {
                                 <span>5</span>
                             </div>
                         </li>
-                        <li className="product-list__item">
-                            <div data-slide="6" className="product">
-                                <span>6</span>
-                            </div>
-                        </li>
+
                     </ul>
                 </div>
             </div>
