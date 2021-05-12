956fab2df01a2c0bcdeeaae98e80ef6a94a9c887
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-05-12 16:43:37 +0200

    Added search function and filtering options

--- src/components/BookCover.jsx
+++ src/components/BookCover.jsx
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom'
 import './BookCover.css'
 export default function BookCover(props) {
     
-    const num = Number(props.number) + 1
-
     return (
-        <div data-slide={num} className="product">
+        <div  className="product">
             <Link to={`/collection/${props.data._id}`}>
 
                 <span>
