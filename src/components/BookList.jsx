4de93ec97ecd0d6e557a2d6746ead7c9191f6811
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-05-12 15:15:19 +0200

    Added structure of BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { DataContext } from '../context/DataContext'
+import BookCover from './BookCover'
+import { getCollection } from '../kit/api/Book'
+import './BookList.css'
 
 export default function BookList() {
+
+    const { collection, setCollection } = useContext(DataContext)
+
+    useEffect(() => {
+        if (!collection) {
+            fetchData()
+        }
+    }, [])
+
+    const fetchData = async () => {
+
+        await getCollection()
+            .then(res => res.json())
+            .then(result => setCollection(result))
+
+    }
+
     return (
         <div>
-            
+            <div className='top-container'>
+                <h1 title='latest-title'>All books</h1>
+                <div></div>
+                <Link id='nav' to='/latest'>
+                    Check my latest reads!
+                </Link>
+            </div>
+           
+            <div id='book-list'>
+                {collection && Object.entries(collection).map((element) => {
+                    return <BookCover data={element[1]} />
+                })
+
+                }
+            </div>
         </div>
+
     )
 }
