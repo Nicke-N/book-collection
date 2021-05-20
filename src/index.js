3f3fbc348023eada268a2841389272a8d7bb5395
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-05-20 15:26:34 +0200

    removed basename from index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 
 ReactDOM.render(
-  <BrowserRouter basename='/book-collection'>
+  <BrowserRouter>
     <React.StrictMode>
       <DataProvider>
         <App />
